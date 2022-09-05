10f556c452b985b21b57114b0e63de8cbf8ae95d
Anže Luzar <aluzarwork@gmail.com>
2022-09-05 09:35:15 +0200

    Update version string to 0.1.9

--- src/iac_scan_runner/api.py
+++ src/iac_scan_runner/api.py
@@ -18,7 +18,7 @@ app = FastAPI(
     docs_url="/swagger",
     title="IaC Scan Runner REST API",
     description="Service that scans your Infrastructure as Code for common vulnerabilities",
-    version="0.1.6",
+    version="0.1.9",
     root_path=os.getenv('ROOT_PATH', "/")
 )
 
