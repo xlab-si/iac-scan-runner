25b1a9715b67d490826815f12fcbef7e349933eb
RedekG <redek.grega@gmail.com>
2023-01-04 10:22:38 +0100

    Update imports

--- src/iac_scan_runner/checks/pyup_safety.py
+++ src/iac_scan_runner/checks/pyup_safety.py
@@ -1,9 +1,9 @@
 from os import listdir
 
 import iac_scan_runner.vars as env
-from iac_scan_runner.check import Check
+from iac_scan_runner.interface.check import Check
 from iac_scan_runner.check_output import CheckOutput
-from iac_scan_runner.check_target_entity_type import CheckTargetEntityType
+from iac_scan_runner.enum.check_target_entity_type import CheckTargetEntityType
 from iac_scan_runner.utils import run_command
 
 
