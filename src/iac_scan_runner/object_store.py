dabca3efcb445e6b2d3c2194bcb3e603b2ecd16c
RedekG <redek.grega@gmail.com>
2023-01-06 11:50:23 +0100

    Add FastAPI class and define API

--- src/iac_scan_runner/object_store.py
+++ src/iac_scan_runner/object_store.py
@@ -1,4 +1,16 @@
 # instantiate runner for scanning IaC
+import os
+
+from fastapi import FastAPI
+
 from iac_scan_runner.functionality.scan_runner import ScanRunner
 
 scan_runner = ScanRunner()
+
+app = FastAPI(
+    docs_url="/swagger",
+    title="IaC Scan Runner REST API",
+    description="Service that scans your Infrastructure as Code for common vulnerabilities",
+    version="0.1.9",
+    root_path=os.getenv('ROOT_PATH', "/")
+)
