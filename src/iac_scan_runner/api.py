fbd425bb1f524f228f76bc8570c90f1f0f2c4074
RedekG <redek.grega@gmail.com>
2023-01-06 11:52:35 +0100

    Remove creation of API instance
    Removing creation of API instance, which is now created in object store module

--- src/iac_scan_runner/api.py
+++ src/iac_scan_runner/api.py
@@ -1,19 +1,10 @@
-import os
-
+import uvicorn
 from content_size_limit_asgi import ContentSizeLimitMiddleware
-from fastapi import FastAPI
 
+from object_store import app
 from iac_scan_runner.object_store import scan_runner
 from iac_scan_runner.routers import scan, project, checks, openapi
 
-# create an API instance
-app = FastAPI(
-    docs_url="/swagger",
-    title="IaC Scan Runner REST API",
-    description="Service that scans your Infrastructure as Code for common vulnerabilities",
-    version="0.1.9",
-    root_path=os.getenv('ROOT_PATH', "/")
-)
 # limit maximum size for file uploads to 50 MB
 app.add_middleware(ContentSizeLimitMiddleware, max_content_size=52428800)
 
@@ -25,3 +16,6 @@ app.include_router(openapi.router)
 app.include_router(project.router)
 app.include_router(scan.router)
 app.include_router(checks.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
