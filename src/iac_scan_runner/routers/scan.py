f21b2ba35fa656f214329df7800dcdf5f650df0f
RedekG <redek.grega@gmail.com>
2023-01-09 08:11:10 +0100

    Add scan model
    Created ScanModel and used it in POST /scan endpoint

--- src/iac_scan_runner/routers/scan.py
+++ src/iac_scan_runner/routers/scan.py
@@ -1,42 +1,39 @@
-from typing import Optional, List, Union
+from typing import Optional, Union
 
 from fastapi import APIRouter
-from fastapi import File, Form, UploadFile, status
+from fastapi import status, Depends
 from fastapi.responses import JSONResponse, HTMLResponse
 
 from iac_scan_runner.enums.scan_response_type import ScanResponseType
-from iac_scan_runner.object_store import scan_runner
 from iac_scan_runner.functionality.results_persistence import ResultsPersistence
+from iac_scan_runner.model.Scan import ScanModel
+from iac_scan_runner.object_store import scan_runner
 
 router = APIRouter(tags=["Scan"], prefix="/scan")
 
 
 @router.post("", summary="Initiate IaC scan", responses={200: {}, 400: {"model": str}})
-async def post_scan(iac: UploadFile = File(..., description='IaC file (zip or tar compressed) that will be scanned'),
+async def post_scan(form_data: ScanModel = Depends(ScanModel.as_form),
                     project_id: Optional[str] = None,
-                    checks: Optional[List[str]] = Form(None,
-                                                       description='List of selected checks (by their unique names) to '
-                                                                   'be executed on IaC'),
                     scan_response_type: ScanResponseType = ScanResponseType.json) -> Union[JSONResponse, HTMLResponse]:
     """
     Run IaC scan
     \f
-    :param iac: IaC file (zip or tar compressed) that will be scanned
+    :param form_data: Form data model
     :param project_id: Identifier of a project_id to which where scan run belongs
-    :param checks: List of selected checks to be executed on IaC
     :param scan_response_type: Scan response type (JSON or HTML)
     :return: JSONResponse or HTMLResponse object (with status code 200 or 400)
     """
     try:
-        if not checks or checks == ['']:
+        if not form_data.checks or form_data.checks == ['']:
             checks = []
         else:
-            checks = list(set(checks[0].split(",")))
+            checks = list(set(form_data.checks[0].split(",")))
 
         if not project_id:
             project_id = ""
 
-        scan_output = scan_runner.scan_iac(iac, project_id, checks, scan_response_type)
+        scan_output = scan_runner.scan_iac(form_data.iac, project_id, checks, scan_response_type)
         if scan_response_type == ScanResponseType.html:
             return HTMLResponse(status_code=status.HTTP_200_OK, content=scan_output)
         return JSONResponse(status_code=status.HTTP_200_OK, content=scan_output)
