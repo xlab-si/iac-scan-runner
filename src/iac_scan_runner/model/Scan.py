bb991c68cb39651001ca7d084ca49c0bb2a98965
RedekG <redek.grega@gmail.com>
2023-01-09 08:11:10 +0100

    Add scan model
    Created ScanModel and used it in POST /scan endpoint

---
+++ src/iac_scan_runner/model/Scan.py
@@ -0,0 +1,27 @@
+from typing import Optional, List
+
+from fastapi import File, Form, UploadFile
+from pydantic import BaseModel
+
+
+class ScanModel(BaseModel):
+    """
+    Scan model
+    """
+    iac: UploadFile
+    checks: Optional[List[str]]
+
+    @classmethod
+    def as_form(cls,
+                checks: Optional[List[str]] = Form(None,
+                                                   description='List of selected checks (by their unique names) to '
+                                                               'be executed on IaC'),
+                iac: UploadFile = File(default=None,
+                                       description='IaC file (zip or tar compressed) that will be scanned')):
+        """
+        Converts model to form_data structure for FastAPI
+
+        :param iac: zip file of documents to be scanned
+        :param checks: List of checks wished to be performed
+        """
+        return cls(checks=checks, iac=iac)
